32f6f7274d1c2c778da6f340689d217fe4f59ec8
Jojo Zhang <jojozhang0327@gmail.com>
2019-10-18 11:49:37 -0400

    complete register, login and authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Bad panda!" });
+      } else {
+        req.user = {
+          username: decodedToken.username,
+        };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
